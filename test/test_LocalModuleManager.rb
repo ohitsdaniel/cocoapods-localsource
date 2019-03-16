28b8866aabe69cfc957fa1f39b9ef4d33fd6ba76
Daniel Peter <daniel.peter@me.com>
2019-03-16 14:55:53 +0100

    [cp-localsource] Readds cocoapods-localsource.rb

--- test/test_LocalModuleManager.rb
+++ test/test_LocalModuleManager.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-require_relative "../lib/cocoapods-localsource/LocalModuleManager.rb"
+require "cocoapods-localsource"
 
 class LocalModuleManagerTest < Minitest::Test
   def setup
